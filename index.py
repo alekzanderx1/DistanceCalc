a1b0f4653ada7b345007165a453021ce6b5d2a15
Ahmad <ahmadtaqi789@gmail.com>
2023-06-13 13:13:00 -0400

    Added code to calculate distance using Haversign formula

--- index.py
+++ index.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request, jsonify, Response
+from math import radians, cos, sin, asin, sqrt
+
 
 # We need to initialise the Flask object to run the flask app 
 # By assigning parameters as static folder name,templates folder name
@@ -24,12 +26,30 @@ def calculateDistance():
         # Read request parameter
         request_data = request.get_json()
         print(f"Request_data: {request_data}")
-        
+        lon1, lat1 = request_data['pointA'].split(',')
+        lon2, lat2 = request_data['pointB'].split(',')
+        distance = haversine(float(lon1), float(lat1), float(lon2), float(lat2))
         result = dict()
-        result['distance'] = 12.0
+        result['distance'] = distance
         return jsonify(result)
     else:
         return Response(
             "Only POST Method allowed",
             status=400,
         )
+    
+def haversine(lon1, lat1, lon2, lat2):
+    """
+    Calculate the great circle distance in kilometers between two points 
+    on the earth (specified in decimal degrees)
+    """
+    # convert decimal degrees to radians 
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+
+    # haversine formula 
+    dlon = lon2 - lon1 
+    dlat = lat2 - lat1 
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a)) 
+    r = 6371 # Radius of earth in kilometers. Use 3956 for miles. Determines return value units.
+    return round(c * r,2)
