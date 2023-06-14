15ef983fad5428a02e9682006902d673a04ae80b
Ahmad <ahmadtaqi789@gmail.com>
2023-06-14 00:04:07 -0400

    Integrated Google Maps API to find coordinates

--- index.py
+++ index.py
@@ -1,11 +1,15 @@
 from flask import Flask, render_template, request, jsonify, Response
 from math import radians, cos, sin, asin, sqrt
+import googlemaps
+import os
 
 
 # We need to initialise the Flask object to run the flask app 
 # By assigning parameters as static folder name,templates folder name
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+GMAPS_API_KEY = os.environ.get('GMAPS_API_KEY')
+gmaps = googlemaps.Client(key=GMAPS_API_KEY)
 
 # Homepage endpoint
 @app.route('/',methods=['GET'])
@@ -18,6 +22,30 @@ def main():
         "Only GET Method allowed",
         status=400,
     )
+  
+# helper api to find latitude and longitude using address via google maps api
+@app.route('/findCoordinates',methods=['POST'])
+def findCoordinates():
+    if request.method=='POST':
+        # Read request parameter
+        request_data = request.get_json()
+        print(f"Request_data: {request_data}")
+        address = request_data['address']
+        
+        # Geocoding an address using gmaps API
+        geocode_result = gmaps.geocode(address)
+        print(f"geocode_result: {geocode_result}")
+        longitude = geocode_result[0]['geometry']['location']['lng']
+        latitude = geocode_result[0]['geometry']['location']['lat']
+      
+        result = dict()
+        result['coordinates'] = str(longitude) + ',' + str(latitude)
+        return jsonify(result)
+    else:
+        return Response(
+            "Only POST Method allowed",
+            status=400,
+        )
   
 # Calculation endpoint
 @app.route('/calculateDistance',methods=['POST'])
